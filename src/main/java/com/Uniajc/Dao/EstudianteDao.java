0e832fe7dbdb66cabaacf18080e593c353dfe9e9
kevin <kevibalant@gmail.com>
2026-04-16 17:51:46 -0500

    Primer commit

---
+++ src/main/java/com/Uniajc/Dao/EstudianteDao.java
@@ -0,0 +1,63 @@
+package com.Uniajc.Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.uniajc.config.ConexionPostgresDatabase;
+import com.uniajc.modelo.Estudiantes;
+
+public class EstudianteDao {
+   
+
+    public void guardar(Estudiante estudiante) {
+               
+        String sql = "INSERT INTO \"practica-mvc\".estudiantes (Name, Lastname, Email) VALUES (?, ?, ?)";
+
+        try (Connection conn = ConexionPostgresDatabase.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            // Establecer los parámetros del PreparedStatement con los datos del estudiante
+            pstmt.setString(1, estudiante.getNombre());
+            pstmt.setString(2, estudiante.getApellido());
+            pstmt.setString(3, estudiante.getEmail());
+
+            // Ejecutar la consulta para insertar el estudiante en la base de datos
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public List<Estudiante> obtenerTodos() {
+        List<Estudiante> estudiantes = new ArrayList<>();
+
+        String sql = "SELECT id, name, lastname, email FROM \"practica-mvc\".estudiantes";
+
+        try (Connection conn = ConexionPostgresDatabase.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Estudiante estudiante = new Estudiante();
+                estudiante.setId(rs.getInt("id"));
+                estudiante.setNombre(rs.getString("name"));
+                estudiante.setApellido(rs.getString("lastname"));
+                estudiante.setEmail(rs.getString("email"));
+                estudiantes.add(estudiante);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return estudiantes;
+    }
+
+    
+}
