f8ed3eabd5195232619766e96180c15d9097f3cb
luisfernandezviveros-cripto <luisfernandezviveros@gmail.com>
2026-04-24 20:20:35 -0500

    Terminando el mvc de Incripcion

---
+++ src/main/java/com/Uniajc/Controlador/ControladorGrupo.java
@@ -0,0 +1,5 @@
+package com.Uniajc.Controlador;
+
+public class ControladorGrupo {
+    
+}
---
+++ src/main/java/com/Uniajc/Modelo/Inscripcion_Curso.java
@@ -0,0 +1,9 @@
+package com.Uniajc.Modelo;
+
+public class Inscripcion_Curso {
+    private int id_inscripcion;
+    private int id_estudiante;
+    private int id_grupo;
+    private float nota_final;
+    private String estado;
+}
